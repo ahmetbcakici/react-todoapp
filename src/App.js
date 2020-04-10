4f9431d810b0a684f41b50dd60a5c2f33a51002b
ahmetbcakici <ahmetbcakici@gmail.com>
2020-04-10 05:11:53 +0300

    styled little

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
 import React, { Component } from 'react'
+import './Style.css';
 
 export default class App extends Component {
 
   state = {
     gorev:'',
-    yapilacaklar:[],
+    yapilacaklar:['ekmek al'],
   }
 
   isimDegistir = () => {
@@ -23,18 +23,19 @@ export default class App extends Component {
     // 2. yöntem spread operatör ile
     // spread operatör = ...
     
-    this.setState({yapilacaklar:[...this.state.yapilacaklar,this.state.gorev]})
+    this.setState({yapilacaklar:[...this.state.yapilacaklar,this.state.gorev],gorev:''});
+
   }
   
   render() {
     return (
       <div style={{textAlign:'center'}}>
         <h1>Yapılacaklar Listesi</h1>
-        <input type="text" onChange={e => this.setState({gorev:e.target.value})}/>
-        <button onClick={this.gorevEkle}>Tamam</button>
+        <input type="text" value={this.state.gorev} onChange={e => this.setState({gorev:e.target.value})}/>
+        <button onClick={this.gorevEkle}>Ekle</button>
         <ul>
           {/* return'ün döneceği yer */}
-          {this.state.yapilacaklar.map(item => <li>{item}</li> )}
+          {this.state.yapilacaklar.map(item => <li className="item">{item} <span>&times;</span></li> )}
         </ul>
       </div>
     )
